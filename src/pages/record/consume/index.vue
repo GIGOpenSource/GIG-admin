faabaa966ae07d2eb9124c318e4a49f018c55630
wyz <wyz03@qq.com>
2025-07-30 13:33:15 +0800

    feat(page/router) 记录查询

---
+++ src/pages/record/consume/index.vue
@@ -0,0 +1,164 @@
+<template>
+  <div class="blogger-crawler-list-container">
+    <t-form :data="formData" :label-width="80">
+      <t-row>
+        <t-col :span="10">
+          <t-row :gutter="[24, 24]">
+            <t-col :span="4">
+              <t-form-item label="选择包" name="id">
+                <t-input
+                  v-model="formData.id"
+                  type="search"
+                  :placeholder="'请输入博主ID'"
+                  :style="{ minWidth: '134px' }"
+                />
+              </t-form-item>
+            </t-col>
+            <t-col :span="4">
+              <t-form-item label="订单类型" name="status">
+                <t-select
+                  v-model="formData.status"
+                  :options="statusOptions"
+                  :placeholder="'请选择状态'"
+                  clearable
+                  :style="{ minWidth: '134px' }"
+                />
+              </t-form-item>
+            </t-col>
+          </t-row>
+        </t-col>
+        <t-col :span="2" >
+          <t-button theme="default" type="submit" :style="{ marginLeft: 'var(--td-comp-margin-s)' }"> 查询 </t-button>
+        </t-col>
+      </t-row>
+    </t-form>
+
+    <div class="table-container">
+      <t-table
+        hover
+        :data="tableData"
+        :columns="COLUMNS"
+        row-key="id"
+      >
+      </t-table>
+    </div>
+
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+import type { PrimaryTableCol, TableRowData } from 'tdesign-vue-next';
+
+
+interface FormData {
+  id: string;
+  status: string | number;
+}
+
+const formData = ref<FormData>({
+  id: '',
+  status: '',
+});
+
+
+const statusOptions = [
+  { label: '全部', value: '' },
+  { label: '正常', value: 1 },
+  { label: '禁用', value: 0 },
+];
+
+const COLUMNS: PrimaryTableCol[] = [
+  {
+    title: '序号',
+    colKey: 'uid',
+    align: 'center',
+    width: 80,
+  },
+  {
+    title: '订单ID',
+    colKey: 'nickname',
+    align: 'left',
+    ellipsis: true,
+  },
+  {
+    title: '用户UID',
+    colKey: 'avatar',
+    align: 'center',
+    width: 60,
+  },
+  {
+    title: '订单类型',
+    colKey: 'worksCount',
+    align: 'center',
+    width: 80,
+  },
+  {
+    title: '消费类型',
+    colKey: 'worksRatio',
+    align: 'center',
+    width: 100,
+  },
+  {
+    title: '消费金额',
+    colKey: 'mobile',
+    align: 'left',
+    ellipsis: true,
+    width: 120,
+  },
+  {
+    title: '支付状态',
+    colKey: 'updateTime',
+    align: 'center',
+    width: 140,
+  },
+  {
+    title: '包名',
+    colKey: 'tags',
+    align: 'left',
+    ellipsis: true,
+  },
+  {
+    title: '订单时间',
+    colKey: 'signature',
+    align: 'left',
+    ellipsis: true,
+  },
+  {
+    title: '状态',
+    colKey: 'followFans',
+    align: 'center',
+    width: 120,
+  },
+];
+
+const tableData = ref<TableRowData[]>([
+  { uid: '10001', homepage: 'https://blogger.com/10001', status: 1 },
+  { uid: '10002', homepage: 'https://blogger.com/10002', status: 0 },
+]);
+
+const handleCreate = () => {
+  // 新建逻辑
+};
+
+const handleEdit = (row: TableRowData) => {
+  // 编辑逻辑
+};
+
+const handleDelete = (row: TableRowData) => {
+  // 删除逻辑
+};
+</script>
+
+<style lang="less" scoped>
+.blogger-crawler-list-container {
+  background-color: var(--td-bg-color-container);
+  padding: var(--td-comp-paddingTB-xxl) var(--td-comp-paddingLR-xxl);
+  border-radius: var(--td-radius-medium);
+
+  .table-container {
+    margin-top: var(--td-comp-margin-xxl);
+  }
+}
+
+</style>
---
+++ src/router/modules/record.ts
@@ -0,0 +1,28 @@
+import { LogoutIcon } from 'tdesign-icons-vue-next';
+import { shallowRef } from 'vue';
+
+import Layout from '@/layouts/index.vue';
+
+export default [
+  {
+    path: '/record',
+    name: 'record',
+    component: Layout,
+    redirect: '/record/recharge',
+    meta: { title: '记录查询', icon: 'user-circle' },
+    children: [
+      {
+        path: 'recharge',
+        name: 'RecordRecharge',
+        component: () => import('@/pages/record/recharge/index.vue'),
+        meta: { title: '充值记录' },
+      },
+      {
+        path: 'consume',
+        name: 'RecordConsume',
+        component: () => import('@/pages/record/consume/index.vue'),
+        meta: { title: '消费记录' },
+      },
+    ],
+  },
+];
